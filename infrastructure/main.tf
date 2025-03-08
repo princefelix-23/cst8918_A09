9293e2da077f4f85f6f67fd11561aca44b5d3170
PrinceAFelix <princefelix23@icloud.com>
2025-03-07 19:43:52 -0500

    Test workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+#   account_replication_type = "LRS"
 }
 
 output "resource_group_name" {
